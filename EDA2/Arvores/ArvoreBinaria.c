c1789f1005eedaa701885820aefeb615d5fc2e07
Marcus Rezende <marcusviniciusxmvr@gmail.com>
2026-03-13 21:04:13 -0300

    novo codigo de arvores binarias

--- EDA2/Arvores/ArvoreBinaria.c
+++ EDA2/Arvores/ArvoreBinaria.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct no{
+typedef struct no
+{
     int conteudo;
     struct no *esquerda, *direita;
 } No;
 
-typedef struct {
+typedef struct
+{
     No *raiz;
 } arvore_binaria;
 
@@ -15,106 +17,120 @@ void inserirDireita(No *no, int valor);
 void inserir(arvore_binaria *arv, int valor);
 void imprimirArvore(No *raiz);
 
-void inserirEsquerda(No *no, int valor){
-    if(no->esquerda == NULL){
+void inserirEsquerda(No *no, int valor)
+{
+    if (no->esquerda == NULL)
+    {
 
-        No *novo = (No*)malloc(sizeof(No));
+        No *novo = (No *)malloc(sizeof(No));
         novo->conteudo = valor;
         novo->esquerda = NULL;
         novo->direita = NULL;
 
         no->esquerda = novo;
     }
-    else{
-        if( no->esquerda->conteudo < valor){
-            inserirEsquerda(no->esquerda, valor);
-        }
-        else{
+    else
+    {
+        if (no->esquerda->conteudo < valor)
+        {
             inserirDireita(no->esquerda, valor);
+        }
+        else
+        {
+            inserirEsquerda(no->esquerda, valor);
         }
     }
 }
-void inserirDireita(No *no, int valor){
-    if (no->direita = NULL){
-        No *novo = (No*)malloc(sizeof(No));
+void inserirDireita(No *no, int valor)
+{
+    if (no->direita == NULL)
+    {
+        No *novo = (No *)malloc(sizeof(No));
         novo->conteudo = valor;
         novo->direita = NULL;
         novo->esquerda = NULL;
 
         no->direita = novo;
     }
-    else{
-        if(no->direita->conteudo > valor){
+    else
+    {
+        if (no->direita->conteudo > valor)
+        {
             inserirEsquerda(no->direita, valor);
         }
-        else{
+        else
+        {
             inserirDireita(no->direita, valor);
         }
     }
 }
 
-void inserir(arvore_binaria *arv, int valor) {
+void inserir(arvore_binaria *arv, int valor)
+{
 
     if (arv->raiz == NULL)
     {
-        No *novo = (No*)malloc(sizeof(No));
+        No *novo = (No *)malloc(sizeof(No));
         novo->conteudo = valor;
         novo->esquerda = NULL;
         novo->direita = NULL;
         arv->raiz = novo;
     }
-    else{
-        if(arv->raiz->conteudo > valor){
+    else
+    {
+        if (arv->raiz->conteudo > valor)
+        {
             inserirEsquerda(arv->raiz, valor);
         }
-        inserirDireita(arv->raiz, valor);
+        else{
+            inserirDireita(arv->raiz, valor);
+        }
     }
 }
-void imprimirArvore(No *raiz){
-    if(raiz != NULL){
-        printf("%d, ", raiz->conteudo);
-        imprimirArvore(raiz->esquerda); // (*raiz).esquerda ----> mesma coisa 
+void imprimirArvore(No *raiz)
+{
+    if (raiz != NULL)
+    {
+        imprimirArvore(raiz->esquerda); // (*raiz).esquerda ----> mesma coisa
+
+        printf("%d ", raiz->conteudo);
+        
         imprimirArvore(raiz->direita);
     }
-    else{
-        printf(" Arvore finalizada ");
-    }
 }
 
-
-int main(){
+int main()
+{
 
     int opcao, valor;
     arvore_binaria arv;
-
     arv.raiz = NULL;
-    do{ 
 
-        printf("\nO - sair\n1 - inserir\n2 - imprimir\n ");
+    do
+    {
+
+        printf("\nO - sair\n1 - inserir\n2 - imprimir\n");
         scanf("%d", &opcao);
 
-        switch (opcao){
+        switch (opcao)
+        {
 
         case 0:
             printf("\n Saindo...\n");
             break;
         case 1:
-            printf("Digite um valor");
+            printf("Digite um valor ");
             scanf("%d", &valor);
             inserir(&arv, valor);
             break;
         case 2:
-            printf("\nImpressão da Arvore Binaria:\n");
+            printf("\nImpressao da Arvore Binaria:\n");
             imprimirArvore(arv.raiz);
             break;
-        
+
         default:
-            printf("\nOpção invalida...\n");
-            break;
+            printf("\nOpcao invalida...\n");
         }
 
-    }while (opcao != 0);
-    
-
-    return 0;
+    } while (opcao != 0);
 }
